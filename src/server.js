dfb90fc4f39fde9a6de2dc563509fabffee40a8c
Aristizabal <aristizabal.ocampo@gmail.com>
2024-11-22 21:10:08 -0500

    probando token bot

--- src/server.js
+++ src/server.js
@@ -3,5 +3,6 @@ const app = require('./app');
 const PORT = process.env.PORT || 6767;
 
 app.listen(PORT, () => {
+    console.log(process.env.DISCORD_TOKEN);
     console.log(`Server is running on http://localhost:${PORT}`);
 });
