8ad0f4a1ea946d2d50d861fd3cf72d0ffe761366
Aristizabal <aristizabal.ocampo@gmail.com>
2024-11-22 20:59:09 -0500

    pruebas cors

--- src/app.js
+++ src/app.js
@@ -12,17 +12,17 @@ const discordRoutes = require('./routes/discordRoutes');
 const app = express();
 
 // solo permite solicitudes desde mi dominio
-app.use(cors({
-    origin: function(origin, callback) {
-        // Si la solicitud proviene de localhost, se permite para pruebas
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('No Autorizado'), false);
-        }
-    },
-    methods: ['GET'],
-}));
+// app.use(cors({
+//     origin: function(origin, callback) {
+//         // Si la solicitud proviene de localhost, se permite para pruebas
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('No Autorizado'), false);
+//         }
+//     },
+//     methods: ['GET'],
+// }));
 
 // Middlewares
 app.use(morgan('dev'));
