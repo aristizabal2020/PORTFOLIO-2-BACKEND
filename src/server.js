ff0906ddceb711cc99004064878cdbb334685aad
Aristizabal <aristizabal.ocampo@gmail.com>
2024-11-24 16:07:47 -0500

    Configurado greenlock SSL Certificate

--- src/server.js
+++ src/server.js
@@ -10,17 +10,13 @@ const PORT = process.env.PORT || 6767;
 // });
 
 // Configurar Greenlock certificado SSL
-// const greenlock = Greenlock.init({
-//     packageRoot: path.join(__dirname, '../'), // Directorio raíz del paquete
-//     configDir: './greenlock.d', // Carpeta donde se guardarán los certificados
-//     maintainerEmail: 'aristizabal.ocampo@gmail.com', // Tu correo para Let's Encrypt
-//     cluster: false, // Úsalo solo en producción si necesitas clúster
-//     staging: false // Cambia a false para producción
-//   });
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+const greenlock = Greenlock.init({
+    packageRoot: path.join(__dirname, '../'), // Directorio raíz del paquete
+    configDir: '../greenlock.d', // Carpeta donde se guardarán los certificados
+    maintainerEmail: 'aristizabal.ocampo@gmail.com', // Tu correo para Let's Encrypt
+    cluster: false, // Úsalo solo en producción si necesitas clúster
+    staging: false // Cambia a false para producción
+  });
 
 // Iniciar servidor HTTPS con Greenlock
-// greenlock.serve(app);
+greenlock.serve(app);
